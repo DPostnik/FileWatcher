a664b641408a41a23fcf1d62f3c4489ccc049e30
dpost <dpost@DESKTOP-7V0E006>
2021-02-03 12:47:36 +0300

    Add project files.

---
+++ project/Models/Client.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace project.Models
+{
+    public class Client
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public decimal Budget { get; set; }
+        [Required]
+        public  int Id { get; set; }
+    }
+}
---
+++ project/Program.cs
@@ -0,0 +1,14 @@
+﻿using project.DA;
+
+namespace project
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            FileInfoContext userContext = new FileInfoContext();
+            
+            userContext.SaveChanges();
+        }
+    }
+}
